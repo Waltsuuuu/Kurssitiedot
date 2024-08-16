c28cb012defc2d57e685724fe9748783daff0e73
Waltsuuu <waltteriheino@gmail.com>
2024-08-16 12:19:27 +0300

    Tehtavat 1.3-1.5

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,3 @@
-const course = 'Half Stack application development'
-const parts = [
-  {
-    name: 'Fundamentals of React',
-    exercises: 10
-  },
-  {
-    name: 'Using props to pass data',
-    exercises: 7
-  },
-  {
-    name: 'State of a component',
-    exercises: 14
-  }
-]
-
 const Header = (props) => {
   return (
     <h1>{props.course}</h1>
@@ -29,9 +13,9 @@ const Part = (props) => {
 const Content = (props) => {
   return (
     <>
-    <Part part={parts[0].name} exercises={parts[0].exercises}/>
-    <Part part={parts[1].name} exercises={parts[1].exercises}/>
-    <Part part={parts[2].name} exercises={parts[2].exercises}/>
+    <Part part={props.parts[0].name} exercises={props.parts[0].exercises}/>
+    <Part part={props.parts[1].name} exercises={props.parts[1].exercises}/>
+    <Part part={props.parts[2].name} exercises={props.parts[2].exercises}/>
     </>
   )
 }
@@ -43,10 +27,25 @@ const Total = (props) => {
 }
 
 const App = () => {
+const course = 'Half Stack application development'
+const parts = [
+  {
+    name: 'Fundamentals of React',
+    exercises: 10
+  },
+  {
+    name: 'Using props to pass data',
+    exercises: 7
+  },
+  {
+    name: 'State of a component',
+    exercises: 14
+  }
+]
   return (
     <div>
       <Header course={course} />
-      <Content />
+      <Content parts={parts}/>
       <Total total={parts[0].exercises + parts[1].exercises + parts[2].exercises}/>
     </div>
   )
