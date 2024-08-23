b1ce4b38575cda20ab53f4dfa816288070a0ff85
Waltsuuu <waltteriheino@gmail.com>
2024-08-23 10:13:42 +0300

    Moved components to seperate files

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,6 @@
 import PropTypes from 'prop-types';
-
-const Header = (props) => {
-  return (
-    <h1>{props.course}</h1>
-  )
-}
-
-Header.propTypes = {
-  course: PropTypes.string
-}
-
-const Part = (props) => {
-  return (
-    <p>{props.part} - {props.exercises} excercises</p>
-  )
-}
-
-Part.propTypes = {
-  part: PropTypes.string,
-  exercises: PropTypes.number,
-}
-
-const Content = (props) => {
-  return (
-    <>
-    <Part part={props.parts[0].name} exercises={props.parts[0].exercises}/>
-    <Part part={props.parts[1].name} exercises={props.parts[1].exercises}/>
-    <Part part={props.parts[2].name} exercises={props.parts[2].exercises}/>
-    </>
-  )
-}
-
-Content.propTypes = {
-  parts: PropTypes.array,
-}
+import Header from './Components/Header'
+import Content from './Components/Content'
 
 const Total = (props) => {
   return (
